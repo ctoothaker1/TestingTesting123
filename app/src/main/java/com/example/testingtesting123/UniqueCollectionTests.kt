8052aaf8e58bc2b065b3a83d17e5a15bf9d9e934
ctoothaker1 <ctoothaker1@gmail.com>
2024-12-04 15:35:04 -0500

    Added several tests and implemented clear function

--- app/src/main/java/com/example/testingtesting123/UniqueCollectionTests.kt
+++ app/src/main/java/com/example/testingtesting123/UniqueCollectionTests.kt
@@ -19,6 +19,55 @@ class UniqueCollectionTests {
         assert(item.name == "Item1")
     }
 
+    @Test
+    fun addUniqueItem() {
+        collection.addItem(Item("Item1"))
+        collection.addItem(Item("item1"))
+
+        assert (collection.size() == 2)
+    }
+
+    @Test
+    fun clearAllItems() {
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("Item2"))
+        collection.addItem(Item("item2"))
+
+        val originalSize = collection.size()
+        collection.clear()
+        val newSize = collection.size();
+
+        assert(originalSize == 2 && newSize == 0) {"Items not cleared"}
+    }
+    @Test
+    fun removeItem(){
+        collection.remove(Item("item1"))
+        assert(collection.size() == 0)
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("item2"))
+        collection.remove(Item("item3"))
+        assert(collection.size() == 2)
+        collection.remove(Item("item1"))
+        assert(collection.size() == 1)
+        assert((collection.get(0)).name == "item2")
+        collection.remove(Item("item2"))
+        assert(collection.size() == 0)
+    }
+    @Test
+    fun getPosition(){
+        collection.addItem(Item("item1"))
+        collection.addItem(Item("item2"))
+        assert((collection.get(0)).name == "item1"){"wrong item in position"}
+        assert((collection.get(0)).name != "item2"){"right item in position"}
+        assert((collection.get(1)).name == "item2"){"wrong item in position"}
+    }
+    @Test
+    fun collectionSize(){
+        collection.addItem(Item("item1"))
+        assert(collection.size() == 1){"Size wrong"}
+        collection.addItem(Item("Item2"))
+        assert(collection.size() == 2){"Size wrong"}
+    }
 
 
 
