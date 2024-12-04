b664a27428c00e6d217f66046e0d11071994abc1
ctoothaker1 <ctoothaker1@gmail.com>
2024-12-04 15:32:54 -0500

    Setup UniqueCollectionTests file

---
+++ app/src/main/java/com/example/testingtesting123/UniqueCollectionTests.kt
@@ -0,0 +1,25 @@
+package com.example.testingtesting123
+
+import org.junit.Before
+import org.junit.Test
+
+class UniqueCollectionTests {
+
+    lateinit var collection: UniqueCollection
+
+    @Before
+    fun setUp() {
+        collection = UniqueCollection()
+    }
+
+    @Test
+    fun addAnItem() {
+        collection.addItem(Item("Item1"))
+        val item = collection.get(0)
+        assert(item.name == "Item1")
+    }
+
+
+
+
+}
